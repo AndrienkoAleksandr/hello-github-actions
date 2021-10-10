a60a980d94a94358d1421478266b4b4b06a4330e
Oleksandr Andriienko <oandriie@redhat.com>
2021-10-11 00:50:29 +0300

    Add DEPENDENCY.md checker
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM registry.access.redhat.com/ubi8/openjdk-11@sha256:b9481cc1bfb3b8c332b5d6f8fe51fc2a6ba997ea0dc0c3b293029a0f9297cca4
+
+WORKDIR /home/jboss
+ADD entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh; mkdir -p github-action
+COPY . github-action
+USER root
+RUN cd github-action; mvn clean install
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ src/main/java/org/eclipse/che/App.java
@@ -0,0 +1,80 @@
+package org.eclipse.che;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.*;
+//import java.util.stream.Collectors;
+
+//import org.commonmark.Extension;
+//import org.commonmark.ext.gfm.tables.TablesExtension;
+//import org.commonmark.node.Node;
+//import org.commonmark.node.Visitor;
+//import org.commonmark.parser.Parser;
+//import org.commonmark.renderer.html.HtmlRenderer;
+//import org.commonmark.renderer.text.CoreTextContentNodeRenderer;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+        String repoPath = System.getenv("GITHUB_WORKSPACE");
+        File goSum = new File(Path.of(repoPath, "go.sum").toUri());
+        Collection<String> goSumDeps = readGoSumFile(goSum);
+//        System.out.println( "Hello World!" + depList);
+
+        String patternString = "\\|\\s+\\[(.*)@.*\\]\\(.*\\)\\s.*";
+        Pattern pattern = Pattern.compile(patternString);
+        File depListFile = new File(Path.of(repoPath,"DEPENDENCIES.md").toUri());
+        List<String> definedDeps = readFile(depListFile);
+
+        List<String> declaredDep = new ArrayList<>();
+        for (String line: definedDeps) {
+            Matcher matcher = pattern.matcher(line);
+            boolean matches = matcher.matches();
+            if (matches) {
+                declaredDep.add(matcher.group(1));
+            }
+        }
+
+        System.out.println("===================");
+        for (String goSumDep: goSumDeps) {
+            if (!declaredDep.contains(goSumDep)) {
+                System.out.println("Add new dependency to DEPENDENCIES.md file" + goSumDep);
+            }
+        }
+    }
+
+    public static Collection<String> readGoSumFile(File file) throws IOException {
+        List<String> lines = new ArrayList<>();
+        InputStream in = new FileInputStream(file);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line.split(" ")[0].split("@")[0]);
+            }
+//            System.out.println(lines.size());
+        }
+
+        Set<String> uniqueLines = new HashSet<>(lines);
+//        System.out.println(uniqueLines.size());
+
+        return uniqueLines;
+    }
+
+    public static List<String> readFile(File file) throws IOException {
+        InputStream in = new FileInputStream(file);
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        return lines;
+    }
+}
